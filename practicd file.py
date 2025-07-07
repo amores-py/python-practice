503f9ab39db516002ed7216c1cac5d124934f68b
amores-py <amareshghosh120@gmail.com>
2025-07-07 18:02:15 +0530

    Add files via upload

---
+++ practicd file.py
@@ -0,0 +1,10 @@
+a=input("enter  the first number ")
+b=input("enter the second number ")
+a=int(a)
+b=int(b)
+print(f"addition {a+b}")
+print(f"subtraction {a-b}")
+print(f"multification {a*b}")
+print(f"division {a/b}")
+
+   
