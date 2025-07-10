1e5af61e88280da00188e30b4934d9d0e817602f
amores-py <amareshghosh120@gmail.com>
2025-07-10 12:33:42 +0530

    practicd file1.py

--- practicd file1.py
+++ practicd file1.py
@@ -15,5 +15,7 @@ else:
 3.   Displays the final sum.
 '''
 'Task 2'
-total=sum(range(1,51))
+total= 0
+for number in range(1, 51):  
+    total += number
 print(f"the sum of numbers from 1 to 50 is :{total}")
