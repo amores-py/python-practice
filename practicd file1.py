63d428ac426702c2b8fc80b8c2ec0af6c6253cdd
amores-py <amareshghosh120@gmail.com>
2025-07-10 12:26:40 +0530

    Add files via upload

---
+++ practicd file1.py
@@ -0,0 +1,19 @@
+#Problem Statement:  Write a Python program that:
+#1. 	Takes an integer input from the user.
+#2. 	Checks whether the number is even or odd using an if-else statement.
+#3. 	Displays the result accordingly.
+'{Task 1}'
+number=int(input("pleace enter the number: "))
+if number % 2==0:
+    print(f"{number} is a even number")
+else:
+    print(f"{number} is a odd number")
+
+'''Problem Statement: Write a Python program that:
+1.   Uses a for loop to iterate over numbers from 1 to 50.
+2.   Calculates the sum of all integers in this range.
+3.   Displays the final sum.
+'''
+'Task 2'
+total=sum(range(1,51))
+print(f"the sum of numbers from 1 to 50 is :{total}")
